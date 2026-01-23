db5bc685c9bdafe75db32ba32bfd22378fccbeb8
Satya <satyaranjannayak799@gmail.com>
2026-01-23 13:09:41 +0530

    Displayed search results in list

--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -1,50 +1,80 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import axios from "axios";
 import { Search } from "neetoicons";
-import { Input } from "neetoui";
+import { Input, NoData } from "neetoui";
+import { isEmpty } from "ramda";
+
+import MovieListItem from "./MovieListItem";
+
+import PageLoader from "../commons/PageLoader";
 
 const ShowMovies = () => {
   const [movies, setMovies] = useState([]);
-  const [searchKey, setSearchKey] = useState("spider");
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchKey, setSearchKey] = useState("");
 
   const baseUrl = process.env.REACT_APP_BASE_URL;
   const apiKey = process.env.REACT_APP_OMDB_API_KEY;
 
+  useEffect(() => {
+    const deBounceFn = setTimeout(() => {
+      fetchMovies();
+    }, 500);
+
+    return () => clearTimeout(deBounceFn);
+  }, [searchKey]);
+
   const fetchMovies = async () => {
+    if (!searchKey.trim()) {
+      setMovies([]);
+      setIsLoading(false);
+
+      return;
+    }
+
+    setIsLoading(true);
     try {
       const url = `${baseUrl}/?s=${searchKey}&apiKey=${apiKey}`;
       const response = await axios.get(url);
-      const result = response?.data?.Search || [];
-      setMovies(result);
-      console.log(result);
-      console.log(movies);
+
+      setMovies(response?.data?.Search || []);
     } catch (error) {
-      console.log("An error Occured", error);
+      console.log("An error Occurred", error);
       setMovies([]);
+    } finally {
+      setIsLoading(false);
     }
   };
 
-  useEffect(() => {
-    const debounceTimerId = setTimeout(() => {
-      fetchMovies();
-
-      return () => clearInterval(debounceTimerId);
-    }, 500);
-  }, [searchKey]);
+  if (isLoading) {
+    return <PageLoader />;
+  }
 
   return (
-    <div className="flex h-screen flex-col">
+    <div className="flex min-h-screen flex-col bg-[#f5f5f5] px-4 py-8">
+      {/* Search Bar */}
       <Input
-        className="mx-8 my-4"
+        className="mb-8 w-full"
         placeholder="Search for movies"
         prefix={<Search />}
         type="search"
         value={searchKey}
-        onChange={e => {
-          setSearchKey(e.target.value);
-        }}
+        onChange={e => setSearchKey(e.target.value)}
       />
+      {isEmpty(movies) ? (
+        <NoData
+          className="flex h-full w-full flex-1 items-center justify-center"
+          description="Search a valid movie name"
+          title="No Movies Found"
+        />
+      ) : (
+        <div className="grid grid-cols-2 justify-items-center gap-y-8 p-4 md:grid-cols-3 lg:grid-cols-4">
+          {movies.map(movie => (
+            <MovieListItem key={movie.imdbID} {...movie} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
