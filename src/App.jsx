8ae039ffa158743bc20344c089093554aee1521d
Satya <satyaranjannayak799@gmail.com>
2026-01-23 23:34:23 +0530

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import PageNotFound from "components/commons/PageNotFound";
 import ShowMovies from "components/MovieList";
+import { Route, Switch } from "react-router-dom";
+import routes from "routes";
 
 import "./App.css";
 
-const App = () => <ShowMovies />;
+const App = () => (
+  <Switch>
+    <Route exact component={ShowMovies} path={routes.root} />
+    <Route exact component={PageNotFound} path="*" />
+  </Switch>
+);
 
 export default App;
